c758b98d16ac00414a69c93008bcaaef1a688672
TheLonelyPanda <poom.chaisiri@gmail.com>
2023-01-31 07:42:11 +0700

    add data servey

--- application/views/private/surveyFrom6.php
+++ application/views/private/surveyFrom6.php
@@ -81,6 +81,7 @@
 					<div class="p-fluid p-formgrid p-grid">
 						<div class="p-field p-col">
 						<label for="wight">1.1 ข้อมูลบุคคลของผู้ตอบแบบสอบถาม </label>
+
 							<div class="row">
 								<label for="1111" class="col-form-label">1.1.1 เพศ &nbsp</label>
 								<label class="radio-inline col-form-label">
@@ -93,6 +94,7 @@
 									<input type="radio" id="11113" name="S1_1_1_1" value="3"> (3) เพศทางเลือก
 								</label>
 							</div>
+
 							<div class="row">
 								<label for="1112" class="col-form-label">1.1.2 อายุ</label>
 								<div class="col-lg-3" >
@@ -102,8 +104,9 @@
 									ปี (เต็มปีบริบูรณ์)
 								</div>
 							</div>
+
 							<div class="row">
-								<label for="1111" class="col-form-label">1.1.3 รายได้</label>
+								<label for="1113" class="col-form-label">1.1.3 รายได้</label>
 								<div class="col-lg-12" >
 									<label class="radio-inline col-form-label  col-lg-3" style="margin-left: 10px;">
 										<input type="radio" id="11131" name="S1_1_1_3" value="1"> (1) ต่ำกว่า 15,000 บาท
@@ -128,6 +131,120 @@
 									</label>
 									<label class="radio-inline col-form-label  col-lg-3">
 										<input type="radio" id="11138" name="S1_1_1_3" value="8"> (8) มากกว่า 75,000 บาท
+									</label>
+								</div>
+							</div>
+
+							<div class="row">
+								<label for="1114" class="col-form-label">1.1.4 อาชีพ</label>
+								<div class="col-lg-12" >
+									<label class="radio-inline col-form-label  col-lg-3" style="margin-left: 10px;">
+										<input type="radio" id="11141" name="S1_1_1_4" value="1"> (1) ข้าราชการ พนักงานรัฐวิสาหกิจ
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11142" name="S1_1_1_4" value="2"> (2) พนักงานเอกชน
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11143" name="S1_1_1_4" value="3"> (3) นักเรียน/นักศึกษา
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11144" name="S1_1_1_4" value="4"> (4) รับจ้างทั่วไป
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11145" name="S1_1_1_4" value="5"> (5) เกษตรกรรม
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11146" name="S1_1_1_4" value="6"> (6) ประกอบธุรกิจ
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11147" name="S1_1_1_4" value="7"> (7) ไม่ได้ประกอบอาชีพ
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11148" name="S1_1_1_4" value="8"> (8) อื่น ๆ...
+									</label>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-form-label inputTextFix" style="margin-left: 10px;">
+									โปรดระบุ
+								</div>
+								<div class="col-lg-4" >
+									<input type="text" class="form-control" name="S1_1_1_4_TEXT" disabled/>
+								</div>
+							</div>
+
+							<div class="row">
+								<label for="1115" class="col-form-label">1.1.5 การศึกษาระดับสูงสุด</label>
+								<div class="col-lg-12" >
+									<label class="radio-inline col-form-label  col-lg-3" style="margin-left: 10px;">
+										<input type="radio" id="11151" name="S1_1_1_5" value="1"> (1) ไม่เคยเข้ารับการศึกษา
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11152" name="S1_1_1_5" value="2"> (2) การศึกษานอกระบบ (กศน.)
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11153" name="S1_1_1_5" value="3"> (3) ประถมศึกษา
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11154" name="S1_1_1_5" value="4"> (4) มัธยมศึกษา
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11155" name="S1_1_1_5" value="5"> (5) ปวช.
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11156" name="S1_1_1_5" value="6"> (6) ปวส.
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11157" name="S1_1_1_5" value="7"> (7) ปริญญาตรี
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11158" name="S1_1_1_5" value="8"> (8) ปริญญาโท
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="11159" name="S1_1_1_5" value="9"> (9) สูงกว่าปริญญาโท
+									</label>
+									<label class="radio-inline col-form-label  col-lg-3">
+										<input type="radio" id="111510" name="S1_1_1_5" value="10"> (10) การศึกษาในรูปแบบอื่น...
+									</label>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-form-label inputTextFix" style="margin-left: 10px;">
+									โปรดระบุ
+								</div>
+								<div class="col-lg-4" >
+									<input type="text" class="form-control" name="S1_1_1_5_TEXT" disabled/>
+								</div>
+							</div>
+
+							<div class="row">
+								<label for="1116" class="col-form-label">1.1.6 ในช่วงระยะเวลา 5 ปี ท่านเคยมาติดต่อหรือรับบริการจากหน่วยงานในกระบวนการยุติธรรม หน่วยงานใด</label>
+								<div class="col-lg-12" >
+									<label class="radio-inline col-form-label  col-lg-4" style="margin-left: 10px;">
+										<input type="radio" id="11161" name="S1_1_1_6" value="1"> (1) เคย...
+									</label>
+									<label class="radio-inline col-form-label  col-lg-4">
+										<input type="radio" id="11162" name="S1_1_1_6" value="2"> (2) ไม่เคย
+									</label>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-form-label inputTextFix" style="margin-left: 10px;">
+									เคย กรุณาระบุหน่วยงาน
+								</div>
+								<div class="col-lg-4" >
+									<input type="text" class="form-control" name="S1_1_1_6_TEXT" disabled/>
+								</div>
+							</div>
+
+							<div class="row">
+								<label for="11161" class="col-form-label" style="margin-left: 15px;">1.1.6.1 ท่านเคยมาติดต่อหรือรับบริการจากหน่วยงานในกระบวนการยุติธรรมในสถานะใด </label>
+								<div class="col-lg-12" >
+									<label class="radio-inline col-form-label  col-lg-12" style="margin-left: 20px;">
+										<input type="radio" id="111611" name="S1_1_1_6_1" value="1" disabled> (1) ผู้มาติดต่อหรือรับบริการโดยตรง ได้แก่ ผู้เสียหาย ผู้กระทำผิด
+									</label>
+									<label class="radio-inline col-form-label  col-lg-12" style="margin-left: 20px;">
+										<input type="radio" id="111612" name="S1_1_1_6_1" value="2" disabled> (2) ผู้มาติดต่อหรือรับบริการโดยอ้อม ได้แก่ ญาติ ผู้เกี่ยวข้องกับผู้เสียหายหรือผู้กระทำผิด
 									</label>
 								</div>
 							</div>
@@ -160,7 +277,39 @@
 	<script>
 		$(document).ready(function() {
 		    App.init();
+			
 		});
+
+		$('input[name="S1_1_1_4"]').on('change', function(){
+			if($(this).val() === '8') {
+				$('input[name="S1_1_1_4_TEXT"]').prop('disabled', false);  
+			}
+			else {
+				$('input[name="S1_1_1_4_TEXT"]').prop('disabled', true);  
+			}
+		});
+
+		$('input[name="S1_1_1_5"]').on('change', function(){
+			if($(this).val() === '10') {
+				$('input[name="S1_1_1_5_TEXT"]').prop('disabled', false);  
+			}
+			else {
+				$('input[name="S1_1_1_5_TEXT"]').prop('disabled', true);  
+			}
+		});
+
+		$('input[name="S1_1_1_6"]').on('change', function(){
+			if($(this).val() === '1') {
+				$('input[name="S1_1_1_6_TEXT"]').prop('disabled', false); 
+				$('input[name="S1_1_1_6_1"]').attr('disabled', false);
+			}
+			else {
+				$('input[name="S1_1_1_6_TEXT"]').prop('disabled', true);  
+				$('input[name="S1_1_1_6_1"]').attr('disabled', true);
+			}
+		});
+
+		
 
 	</script>
 </body>
